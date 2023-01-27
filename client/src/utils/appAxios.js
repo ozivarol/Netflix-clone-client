796bf8d1bd816dd42b3f94514d650126559f21a0
ozivarol <oguzhanvaroll43@gmail.com>
2023-01-28 00:21:28 +0300

    fixed

--- client/src/utils/appAxios.js
+++ client/src/utils/appAxios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const appAxios = axios.create({
-    baseURL: "https://netflix-clone-api-v2-git-oguzhanvaroll43-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com"
+    baseURL: "https://netflix-clone-api-ms-user.onrender.com"
 })
